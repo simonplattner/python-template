3b89f6da5d3e1620b0fea18455212ebfbda4239d
Simon Plattner <5527585+simonplattner@users.noreply.github.com>
2023-04-28 21:35:34 +0200

    add files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="MyApp",
+    description="Description of my app",
+    version="0.1.0",
+    author="Simon Plattner",
+    author_email="5527585+simonplattner@users.noreply.github.com",
+    packages=find_packages(),
+    install_requires=[],
+)
